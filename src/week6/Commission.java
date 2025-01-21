c49514cd43df079a3ae35f255a0f5ebce53b4710
pawarit <pawarit.pit@ku.th>
2025-01-21 20:05:05 +0700

    commit

---
+++ src/week6/Commission.java
@@ -0,0 +1,26 @@
+package week6;
+
+public class Commission extends Hourly {
+    private double totalSales;
+    private double commissionRate;
+
+    public Commission(String eName, String eAddress, String ePhone, String socialSecNumber, double rate, double commissionRate) {
+        super(eName, eAddress, ePhone, socialSecNumber, rate);
+        this.totalSales = 0.0;
+        this.commissionRate = commissionRate;
+    }
+    public void addSales(double totalSales) {
+        this.totalSales += totalSales;
+    }
+    @Override
+    public double pay() {
+        double basePay = super.pay();
+        double commissionPay = totalSales * commissionRate;
+        totalSales = 0.0;
+        return basePay + commissionPay;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "\nTotal Sales: " + totalSales;
+    }
+}
---
+++ src/week6/Executive.java
@@ -0,0 +1,20 @@
+package week6;
+
+public class Executive extends Employee {
+    private double bonus;
+
+    public Executive(String eName, String eAddress, String ePhone, String socialSecNumber, double rate) {
+        super(eName, eAddress, ePhone, socialSecNumber, rate);
+        bonus = 0;
+    }
+
+    public void awardBonus (double execBonus) {
+        bonus = execBonus;
+    }
+
+    public double pay() {
+        double payment = super.pay();
+        bonus = 0;
+        return payment;
+    }
+}
