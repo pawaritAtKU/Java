6ca35d762441754a0198b7355dc361720352819e
pawarit <pawarit.pit@ku.th>
2025-01-21 18:13:53 +0700

    commit

---
+++ src/week7/ReveresStr.java
@@ -0,0 +1,14 @@
+package week7;
+
+public class ReveresStr {
+    public static void main(String[] args) {
+        String str = "ABCD";
+        System.out.println("Before reveres a String " + str);
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        System.out.println("After reveres a String " + rev);
+    }
+
+}
---
+++ src/week7/SwapNumber.java
@@ -0,0 +1,22 @@
+package week7;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        System.out.println("Before Swapping: " + a + " " + b + " " + c + " " + d);
+
+        int temp = a;
+        a = d;
+        d = temp;
+        temp = b;
+        b = c;
+        c = temp;
+
+        System.out.println("After Swapping: " + a + " " + b + " " + c + " " + d);
+    }
+}
+
